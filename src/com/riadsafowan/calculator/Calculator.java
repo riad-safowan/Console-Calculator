9f1ce440805857e4073f050b301eff2706eb0076
Riad Safowan <riad.safowan@northsouth.edu>
2021-10-31 23:33:39 +0600

    Basic Calculator

--- src/com/riadsafowan/calculator/Calculator.java
+++ src/com/riadsafowan/calculator/Calculator.java
@@ -5,32 +5,42 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Java Calculator v0.1 \n");
-        Scanner Scan = new Scanner(System.in);
+        System.out.println(" Welcome to Java Calculator \n");
+        Scanner scan = new Scanner(System.in);
 
 
         System.out.println("\n Please enter two numbers");
         int xe;
         int xo;
         System.out.print("\n First number: ");
-        xe = Scan.nextInt();
+        xe = scan.nextInt();
         System.out.print("\n Second number: ");
-        xo = Scan.nextInt();
+        xo = scan.nextInt();
 
         System.out.println("\n Select between (*,/,+,-)\n Type out the character in a single letter: ");
-        String Operation = Scan.next();
+        String Operation = scan.next();
         String EO = "You have selected ";
 
         switch (Operation) {
 
-            case "*": System.out.println(EO + "* \n Your Result: "+( xe * xo )); break;
-            case "/": System.out.println(EO + "/ \n Your Result: "+ ( xe / xo )); break;
-            case "+": System.out.println(EO + "+ \n Your Result: "+ ( xe + xo ));break;
-            case "-": System.out.println(EO + "- \n Your Result: "+( xe - xo )); break;
-            default: System.out.println("\n Please select a valid character"); }
+            case "*":
+                System.out.println(EO + "* \n Your Result: " + (xe * xo));
+                break;
+            case "/":
+                System.out.println(EO + "/ \n Your Result: " + (xe / xo));
+                break;
+            case "+":
+                System.out.println(EO + "+ \n Your Result: " + (xe + xo));
+                break;
+            case "-":
+                System.out.println(EO + "- \n Your Result: " + (xe - xo));
+                break;
+            default:
+                System.out.println("\n Please select a valid character");
+        }
 
         //Close
-        Scan.close();
+        scan.close();
         System.out.println(" Closing Application ");
     }
 }

23d9212c6c89d3b072b48d2e5433266d1a61bcb4
Riad Safowan <riad.safowan@northsouth.edu>
2021-11-01 19:38:04 +0600

    Circle and Sin(x) added

--- src/com/riadsafowan/calculator/Graph.java
+++ src/com/riadsafowan/calculator/Graph.java
@@ -10,12 +10,16 @@ public class Graph {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Point> points = new ArrayList<>();
-        float m, a, b;
+        float m, r, a, b;
+        char ch;
         boolean isDone = false;
+        boolean hasGraph = false;
         while (!isDone) {
             System.out.println("1 -> y=mx+b");
             System.out.println("2 -> y=m(x+a)^2+b");
             System.out.println("3 -> y=m(x+a)^3+b");
+            System.out.println("4 -> y=+-(r^2-x^2)^.5");
+            System.out.println("5 -> y=sin(x)");
             System.out.println("0 -> Close application");
 
             System.out.print("Select: ");
@@ -26,10 +30,17 @@ public class Graph {
                     m = scanner.nextFloat();
                     System.out.print("Enter b: ");
                     b = scanner.nextFloat();
-                    points.clear();
+                    if (hasGraph) {
+                        System.out.print("Do you want to keep the old graph? y/n : ");
+                        ch = scanner.next().charAt(0);
+                        if (ch == 'n' || ch == 'N') {
+                            points.clear();
+                        }
+                    }
                     for (int i = -X / 2; i < X / 2; i++) {
                         points.add(new Point(i, (int) (m * i + b)));
                     }
+                    hasGraph = true;
                     drawArray(points);
                     break;
                 }
@@ -40,10 +51,17 @@ public class Graph {
                     a = scanner.nextFloat();
                     System.out.print("Enter b: ");
                     b = scanner.nextFloat();
-                    points.clear();
+                    if (hasGraph) {
+                        System.out.print("Do you want to keep the old graph? y/n : ");
+                        ch = scanner.next().charAt(0);
+                        if (ch == 'n' || ch == 'N') {
+                            points.clear();
+                        }
+                    }
                     for (int i = -X / 2; i < X / 2; i++) {
                         points.add(new Point(i, (int) (m * Math.pow(i + a, 2) + b)));
                     }
+                    hasGraph = true;
                     drawArray(points);
                     break;
                 }
@@ -54,10 +72,56 @@ public class Graph {
                     a = scanner.nextFloat();
                     System.out.print("Enter b: ");
                     b = scanner.nextFloat();
-                    points.clear();
+                    if (hasGraph) {
+                        System.out.print("Do you want to keep the old graph? y/n : ");
+                        ch = scanner.next().charAt(0);
+                        if (ch == 'n' || ch == 'N') {
+                            points.clear();
+                        }
+                    }
                     for (int i = -X / 2; i < X / 2; i++) {
                         points.add(new Point(i, (int) (m * Math.pow(i + a, 3) + b)));
                     }
+                    hasGraph = true;
+                    drawArray(points);
+                    break;
+                }
+                case 4: {
+                    System.out.print("Enter r: ");
+                    r = scanner.nextFloat();
+                    System.out.print("Enter a: ");
+                    a = scanner.nextFloat();
+                    System.out.print("Enter b: ");
+                    b = scanner.nextFloat();
+                    if (hasGraph) {
+                        System.out.print("Do you want to keep the old graph? y/n : ");
+                        ch = scanner.next().charAt(0);
+                        if (ch == 'n' || ch == 'N') {
+                            points.clear();
+                        }
+                    }
+                    for (int i = -X / 2; i < X / 2; i++) {
+                        if (i >= -r && i <= r) {
+                            points.add(new Point((int) (i - a), (int) ((int) (Math.pow(Math.pow(r, 2) - Math.pow((i), 2), 0.5)) + b))); //y=+-(r^2-x^2)^.5"
+                            points.add(new Point((int) (i - a), (int) ((-1 * (int) (Math.pow(Math.pow(r, 2) - Math.pow((i), 2), 0.5))) + b))); //y=+-(r^2-x^2)^.5"
+                        }
+                    }
+                    hasGraph = true;
+                    drawArray(points);
+                    break;
+                }
+                case 5: {
+                    if (hasGraph) {
+                        System.out.print("Do you want to keep the old graph? y/n : ");
+                        ch = scanner.next().charAt(0);
+                        if (ch == 'n' || ch == 'N') {
+                            points.clear();
+                        }
+                    }
+                    for (int i = -X / 2; i < X / 2; i++) {
+                        points.add(new Point(i, (int) (10 * Math.sin(25 * i * 180 / Math.PI))));
+                    }
+                    hasGraph = true;
                     drawArray(points);
                     break;
                 }
@@ -77,30 +141,30 @@ public class Graph {
             for (int j = 0; j < X; j++) {
                 x = j - Y / 2;
                 y = X / 2 - i;
-                for (int p = 0; p < points.size(); p++) {
-                    if (points.get(p).x == x) {
-                        if (points.get(p).y == y) {
+                for (Point point : points) {
+                    if (point.x == x) {
+                        if (point.y == y) {
                             isPoint = true;
                         }
                     }
                 }
                 if (isPoint) {
-                    System.out.print("$ ");
+                    System.out.print("$  ");
                     isPoint = false;
                 } else if (i == Y / 2 && j == X / 2) {
-                    System.out.print("0 ");
+                    System.out.print("0  ");
                 } else if (j == X / 2 || i == Y / 2) {
                     if (j % 10 == 0 && j != X / 2) {
-                        System.out.print("| ");
+                        System.out.print("|  ");
                     } else {
                         if (i % 10 == 0 && i != Y / 2) {
-                            System.out.print("@ ");
+                            System.out.print("@  ");
                         } else {
-                            System.out.print("* ");
+                            System.out.print("*  ");
                         }
                     }
                 } else {
-                    System.out.print("  ");
+                    System.out.print("   ");
                 }
             }
             System.out.println();
